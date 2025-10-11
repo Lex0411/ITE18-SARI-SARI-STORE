f29869a672feddb1cecdb408793509b9b4bae729
notechyb11 <albacietep@gmail.com>
2025-10-11 22:25:11 +0800

    preload.js

--- preload.js
+++ preload.js
@@ -2,10 +2,14 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  // File operations
+  readFile: (filename) => ipcRenderer.invoke('read-file', filename),
+  writeFile: (filename, data) => ipcRenderer.invoke('write-file', filename, data),
+  fileExists: (filename) => ipcRenderer.invoke('file-exists', filename),
+});
+
+contextBridge.exposeInMainWorld('electronAPI', Object.freeze({
   readFile: (filename) => ipcRenderer.invoke('read-file', filename),
   writeFile: (filename, data) => ipcRenderer.invoke('write-file', filename, data),
   fileExists: (filename) => ipcRenderer.invoke('file-exists', filename),
   loadAllData: () => ipcRenderer.invoke('load-all-data'),
-
-});
+}));
